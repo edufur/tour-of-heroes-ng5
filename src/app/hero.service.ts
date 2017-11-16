6868635638996e1e6606c9dc13729a1f864745ea
Thiago Reis <thiagoreis128@gmail.com>
2017-11-16 13:36:34 -0200

    Inject it into the HeroService

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -3,11 +3,11 @@ import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
-
+import { MessageService } from './message.service';
 @Injectable()
 export class HeroService {
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 
  getHeroes(): Observable<Hero[]> {
   return of(HEROES);
