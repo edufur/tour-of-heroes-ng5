6758e07156f0a7485f0e342cb13e4cb752796103
Thiago Reis <thiagoreis128@gmail.com>
2017-11-20 14:24:49 -0200

    Tap into the Observable

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -20,6 +20,7 @@ export class HeroService {
 getHeroes (): Observable<Hero[]> {
   return this.http.get<Hero[]>(this.heroesUrl)
     .pipe(
+    	tap(heroes => this.log(`feched heroes`) ),
       catchError(this.handleError('getHeroes', []))
     );
 }
