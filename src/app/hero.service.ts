89ba2510c6e54778502883385e93abc4a276524b
Thiago Reis <thiagoreis128@gmail.com>
2017-11-21 10:54:07 -0200

    Add HeroService.addHero()

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -38,6 +38,13 @@ updateHero (hero: Hero): Observable<any> {
   );
 }
 
+add(hero: Hero): Observable<Hero> {
+  return this.http.post<Hero>(this.heroesUrl, hero, httpOptions).pipe(
+    tap((hero: Hero) => this.log(`added hero w/ id=${hero.id}`)),
+    catchError(this.handleError<Hero>("addHero"))
+  );
+}
+
 private log(message: string){
 	this.messageService.add('HeroService: ' + message);
 }
